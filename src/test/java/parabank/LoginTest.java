4082f1bfa9000ef19e3d6a5156e428425375e34b
mirian228 <pariksini@mail.ru>
2023-03-01 17:10:09 +0400

    get commands

--- src/test/java/parabank/LoginTest.java
+++ src/test/java/parabank/LoginTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest {
---
+++ src/test/java/webdrivercommands/GetCommands.java
@@ -0,0 +1,24 @@
+package webdrivercommands;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class GetCommands {
+	WebDriver driver;
+
+	@Test()
+	public void getCommandsTest() {
+		System.setProperty("webdriver.firefox.marionette", "C:\\Dev\\Drivers\\firefox\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://facebook.com"); // Opens URL
+		System.out.println(driver.getTitle()); // returns page title
+		System.out.println(driver.getCurrentUrl()); // returns page current URL
+		WebElement text = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[1]/h2"));
+		System.out.println(text.getText()); // Returns text from page element
+
+	}
+
+}
